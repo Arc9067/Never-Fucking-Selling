d68f0cc116eeeb283a18e8c633616088f4445036
Mikail Rokib <atandarokib@gmail.com>
2023-05-27 18:46:59 +0100

    up

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,13 +7,14 @@ const Footer = () => {
     <footer className="py-[30px] w-full bg-secondary">
       <div className="container flex justify-center items-center gap-6">
         <a href="https://mobile.twitter.com/NeverFkingSell">
-          {" "}
           <FaTwitter className="text-primary" size={52} />
         </a>
         <a href="https://t.me/NeverfuckingSelling">
           <FaTelegramPlane className="text-primary" size={52} />
         </a>
-        <MdMail className="text-primary" size={52} />
+        <a href="mailto:Yugifouga@gmail.com">
+          <MdMail className="text-primary" size={52} />
+        </a>
       </div>
     </footer>
   );
